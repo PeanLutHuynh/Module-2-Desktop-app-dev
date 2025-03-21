ac5fd5dc65dd5cc59c5b15038424796eebe1b876
Gia Lạc <metfc1.0@gmail.com>
2025-03-21 23:22:19 +0700

    Add project files.

---
+++ Module2BaiSo1HuynhNhatGiaLac/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Module2BaiSo1HuynhNhatGiaLac")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Module2BaiSo1HuynhNhatGiaLac")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6f8fca04-8bc5-40d6-8ff3-f18de27dda21")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Module2BaiSo1HuynhNhatGiaLac/SimpleEventExampleForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module2BaiSo1HuynhNhatGiaLac
+{
+    public partial class SimpleEventExampleForm : Form
+    {
+        public SimpleEventExampleForm()
+        {
+            InitializeComponent();
+            SimpleEventExampleForm_Load(this, EventArgs.Empty);
+        }
+
+        private void SimpleEventExampleForm_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Form was clicked.");
+        }
+
+        private void btnClickMe_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button was clicked.");
+        }
+
+        private void SimpleEventExampleForm_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hi , Welcome to C# 2010 programming!");
+        }
+    }
+}
